344925cf0f23af5598b1e9e9e2cb81dc98d98795
Jason <iyoujia.zheng@gmail.com>
2017-09-18 22:18:23 +1000

    from t430

--- hbeauty.aspnetcore2/ClientApp/app/components/service/service.component.ts
+++ hbeauty.aspnetcore2/ClientApp/app/components/service/service.component.ts
@@ -1,10 +1,24 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { SharedComponent } from '../shared/shared.component';
 
+//import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 @Component({
     selector: 'service',
     templateUrl: './service.component.html'
 })
 export class ServiceComponent {
 
+    public item1 = 100;
+    //items: FirebaseListObservable<any[]>;
+
+    //constructor(private db: AngularFireDatabase) {
+
+    //}
+
+    ngOnInit() {
+        //this.items = this.db.list('/services');
+        //console.log(this.db);
+        console.log('????????????????????????????????????')
+    }
 }
