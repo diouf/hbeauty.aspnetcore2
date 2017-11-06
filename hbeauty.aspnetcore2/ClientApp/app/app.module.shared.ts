a48eb546aed3a06549cc35916a88856d25b32ba4
Jason <iyoujia.zheng@gmail.com>
2017-11-07 00:24:27 +1100

    break down serviceItem component. generate routers on-the-fly. getting paramaters from router.

--- hbeauty.aspnetcore2/ClientApp/app/app.module.shared.ts
+++ hbeauty.aspnetcore2/ClientApp/app/app.module.shared.ts
@@ -58,19 +58,18 @@ import { Summary } from './pipes/Summary';
         CommonModule,
         HttpModule,
         FormsModule,
-        ///admin/serviceitems
+        
         RouterModule.forRoot([
             { path: '', component:HomeComponent},
 
             { path: 'ad', component:AdminComponent,
-              children:[{
-                  path: 'serviceitems', component: AdminServiceItemComponent
-              }]
+              children:[
+                  {path: 'serviceitems', component: AdminServiceItemComponent},
+                  {path: 'serviceitems/:serviceItemId', component: AdminEditServiceItemComponent},
+                ]
             },
-            
-            { path:'h-admin',component:AdminServiceItemComponent},
-            { path:'asd-edit-item',component:AdminEditServiceItemComponent},
-            { path: 'dion', redirectTo:'h-admin'},
+
+            { path: 'dion', redirectTo:'ad'},
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo:''}
         ]),
